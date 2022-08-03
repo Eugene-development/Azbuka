bca3353905a76d283fb168519d93de90d83d79a2
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-04 00:05:15 +0300

    git init

--- app/src/routes/shkafy/index.svelte
+++ app/src/routes/shkafy/index.svelte
@@ -1,7 +1,8 @@
 <script>
-    import {Header_v3} from "symax/ui/broker";
+    import {Header_v3, Marketing_v3} from "symax/ui/broker";
     export let content_Header_v3
+    export let content_Marketing_v3
 </script>
 
 <Header_v3 {...content_Header_v3}/>
-
+<Marketing_v3 {...content_Marketing_v3}/>
