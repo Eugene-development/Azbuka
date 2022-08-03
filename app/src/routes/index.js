662143c27a9496501b8503077800b54b0968e9d9
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-03 12:42:08 +0300

    git init

---
+++ app/src/routes/index.js
@@ -0,0 +1,21 @@
+const text = {
+    mobile: 'Снижены цены на продукцию.',
+    desktop: 'Хорошие новости! Снижены цены на продукцию.'
+};
+const link = {
+    value: 'Подробнее ->',
+    path: '#'
+};
+
+const content_Banner_v1 = {
+    text,
+    link
+};
+
+export const GET = () => {
+    return {
+        body: {
+            content_Banner_v1
+        }
+    };
+};
---
+++ app/src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {Banner_v1} from "symax/ui/broker"
+    export let content_Banner_v1
+</script>
+
+<Banner_v1 {...content_Banner_v1}/>
