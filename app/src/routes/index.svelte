57d1211a5fcb4f3ed4420b16a206ff5610d8abb8
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-03 21:39:26 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script>
-    import {Blog_v1, Main_v1, Marketing_v1} from "symax/ui/broker"
+    import {Blog_v1, Main_v1, Marketing_v1, Marketing_v2} from "symax/ui/broker"
     export let contentMain_v1
     export let contentMarketing_v1
     export let contentBlog_v1
+    export let contentMarketing_v2
 </script>
 
 <Main_v1 {...contentMain_v1}/>
 <Marketing_v1 {...contentMarketing_v1}/>
+<Marketing_v2 {...contentMarketing_v2}/>
 <Blog_v1 {...contentBlog_v1}/>
