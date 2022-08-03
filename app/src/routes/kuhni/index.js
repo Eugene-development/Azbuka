a541b8c390d67a07ba3ce6f552f90df76992a3af
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-04 00:09:04 +0300

    git init

--- app/src/routes/kuhni/index.js
+++ app/src/routes/kuhni/index.js
@@ -1,7 +1,7 @@
 const title = 'Кухни в Нижнем Новгороде';
 const description = 'Кухонные гарнитуры в Нижнем Новгороде. Дизайн проект в подарок.';
 const pathImg =
-    'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/1.jpg';
+    'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/kitchens/4.jpg';
 
 const content_Header_v3 = {
     title,
