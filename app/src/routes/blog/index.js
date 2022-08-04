1dd09ea8e4b7cfc563936bc2b3fd2c20dc783898
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-04 22:39:16 +0300

    git init

--- app/src/routes/blog/index.js
+++ app/src/routes/blog/index.js
@@ -1,48 +1,8 @@
-const title_Blog_v1 = 'Lorem ipsum dolor sit amet';
-const description_Blog_v1 =
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!';
+import {posts as posts_Blog_v1} from "./index.json"
 
-const posts_Blog_v1 = [
-    {
-        id: '#',
-        img: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV1/kitchen.jpeg',
-        alt: '',
-        title: 'From the blog\n',
-        description:
-            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed',
-        avatar:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        author: 'Roel Aufderehar',
-        date: 'Mar 16, 2020'
-    },
-    {
-        id: '#',
-        img: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV1/kitchen.jpeg',
-        alt: '',
-        title: 'From the blog\n',
-        description:
-            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed',
-        avatar:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        author: 'Roel Aufderehar',
-        date: 'Mar 16, 2020'
-    },
-    {
-        id: '#',
-        img: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV1/kitchen.jpeg',
-        alt: '',
-        title: 'From the blog\n',
-        description:
-            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed',
-        avatar:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        author: 'Roel Aufderehar',
-        date: 'Mar 16, 2020'
-    }
-];
 const contentBlog_v1 = {
-    title_Blog_v1,
-    description_Blog_v1,
+    title_Blog_v1: 'Наш блог',
+    description_Blog_v1: 'Информационные статьи на тему мебельной индустрии от отечественных и зарубежных авторов',
     posts_Blog_v1
 };
 export const GET = () => {
