5d6cb9246e83899e7e0da325ff53359bed68b642
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-06 20:09:57 +0300

    git init

--- app/src/routes/information/podem/index.js
+++ app/src/routes/information/podem/index.js
@@ -1,5 +1,5 @@
 const title_Marketing_v5 = 'Подъём'
-const text_Marketing_v5 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus eligendi enim et in laborum, magni natus nesciunt officiis omnis quibusdam rem velit vitae voluptatibus.'
+const text_Marketing_v5 = 'Поднимем ваш заказ по лестничным пролётам или на лифте максимально аккуратно и бережно. Услуга платная. Точную цену уточняйте при оформлении договора.'
 const img_Marketing_v5 = 'https://images.unsplash.com/photo-1507207611509-ec012433ff52?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80'
 
 const content_Marketing_v5 = {
