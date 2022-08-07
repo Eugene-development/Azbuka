97cbd536272581ab4584acafca846e5f9fdbfac3
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-07 19:35:14 +0300

    git init

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -5,11 +5,13 @@
     import {content_Header_v1} from "../content/header/section_v1/index.js";
     import {content_Header_v2} from "../content/header/section_v2/index.js";
     import {content_Banner_v1} from "../content/banner/section_v1/index.js";
+    import {content_Modal_v2} from "../content/modal/v2/index.js";
 
     const {content: header_v1} = content_Header_v1
     const {content: header_v2} = content_Header_v2
     const {content: footer_v1} = content_Footer_v1
     const {content: banner_v1} = content_Banner_v1
+    const {content: modal_v2} = content_Modal_v2
 </script>
 
 <!--<Banner_v1 {...banner_v1}/>-->
@@ -20,4 +22,4 @@
 </main>
 <Footer_v1 {...footer_v1}/>
 <Modal_v1/>
-<Modal_v2/>
+<Modal_v2 {...modal_v2}/>
