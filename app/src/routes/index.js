494f3f869c4759d26b188cfb56014cab1141b3d9
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-19 16:54:29 +0300

    git init

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -114,7 +114,7 @@ const posts_Blog_v1 = [
         alt: "",
         title: "Шкафы купе с фотопечатью на стекле",
         description: "Компании зачастую предлагают вам изготовление шкафов-купе и встроенных шкафов с дверками с фотопечатью. Это...",
-        avatar: "https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/shkafi/komfort-4.jpeg",
+        avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
         author: "Евгений Че",
         date: "6 июля, 2022"
     }
