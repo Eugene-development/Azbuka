c6c35cdd83a8cf4f477901e68b03084cb16ce9ed
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-06 18:19:51 +0300

    init

--- app/src/routes/shkafy/vstroenie/index.svelte
+++ app/src/routes/shkafy/vstroenie/index.svelte
@@ -1,9 +1,9 @@
 <script>
-    import {Marketing_v4, Header_v3} from "symax/ui/broker";
-    export let content_Header_v3
-    export let content_Marketing_v4
+	import { Marketing_v4, Header_v3 } from 'symax/ui/broker';
+	export let content_Header_v3;
+	export let content_Marketing_v4;
 </script>
 
-<Header_v3 {...content_Header_v3}/>
+<Header_v3 {...content_Header_v3} />
 
-<Marketing_v4 {...content_Marketing_v4}/>
+<Marketing_v4 {...content_Marketing_v4} />
